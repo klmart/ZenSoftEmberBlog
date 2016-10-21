4ee069d723227acf98573264429037619b0afa1e
klmart <kulbaev.mart@gmail.com>
2016-10-21 16:03:06 +0600

    Realize new and edit for comment

--- app/routes/comments/new.js
+++ app/routes/comments/new.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return this.store.createRecord('comment')
+  },
+
+  actions: {
+    saveComment(newComment){
+      newComment.save().then(() => { this.transitionTo('comments')});
+    }
+  },
+
+  willTransition(){
+    this.controller.get('model').rollbackAttributes();
+  }
 });
