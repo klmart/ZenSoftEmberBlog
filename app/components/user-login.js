550d1cbc7bcd7daf494c585412148317a1f15f14
klmart <kulbaev.mart@gmail.com>
2016-10-27 10:27:21 +0600

    Delete some lines

--- app/components/user-login.js
+++ app/components/user-login.js
@@ -6,15 +6,10 @@ export default Ember.Component.extend({
     login() {
 
       const  { email, password } = this.getProperties('email', 'password');
-
-      console.log('Запускаю findUser, чтобы найти юзера из базы');
       this.loginService.findUser(email, password).then((user) =>{
         this.loginService.setCurrentUser(user);
-        console.log('Проверь email');
-        console.log(this.get('loginService.currentUser.email'));
       });
 
-      console.log('Должен появться после findUser');
       this.sendAction();
 
     }
--- app/routes/comments/index.js
+++ app/routes/comments/index.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    // return this.store.findAll('comment');
     return this.modelFor('posts.show').get('comments');
   },
 
