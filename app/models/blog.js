199b70c0f2ccc1b7dd498ccf24d7af732a52027c
klmart <kulbaev.mart@gmail.com>
2016-11-01 16:02:52 +0600

    Fix bugs

--- app/models/blog.js
+++ app/models/blog.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
 
@@ -12,7 +13,9 @@ export default DS.Model.extend({
   isValid: Ember.computed.notEmpty('name'),
 
 
-  // checkUser: Ember.computed.equal(this.get('loginService.currentUser.id'), this.get('user.id')),
+  //checkUser: Ember.computed.equal(this.get('loginService.currentUser.id'), this.get('user.id')),
+ // Не получилось реализовать, я так понял this обращается куда то в computed, а не в модель.
+
   checkUser: Ember.computed(function () {
     return this.get('user.id') === this.get('loginService.currentUser.id')? true : false;
   })
