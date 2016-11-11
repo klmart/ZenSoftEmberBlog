e79e0872748dfef65ec9c66500337abb84fa51d9
klmart <kulbaev.mart@gmail.com>
2016-11-11 13:55:24 +0600

    Get user permissions in loginService

--- app/services/login-service.js
+++ app/services/login-service.js
@@ -4,19 +4,23 @@ export default Ember.Service.extend({
 
   store: Ember.inject.service('store'),
   currentUser: Ember.Object.create(),
+  permissions: undefined,
+
   isAuthenticated: Ember.computed('currentUser.id', function () {
     return !!this.get('currentUser.id');
   }),
 
   findUser(email, pass){
     return this.get('store').query('user', {
-       email: email, password: pass }).then(function(users){
+      email: email, password: pass
+    }).then(function (users) {
       return users.get('lastObject');
     });
   },
-
+  
   setCurrentUser(user){
     this.set('currentUser', user);
+    this.set('permissions', user.get('role.permissions'))
   },
 
   setLastLoginDate(user){
