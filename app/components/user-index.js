a11da825a7d3fbeae196a6284c28af1e3aab4956
klmart <kulbaev.mart@gmail.com>
2016-11-11 10:52:44 +0600

    Revert blog-index component, and rename to blog-table. Fix select bug in user/index

--- app/components/user-index.js
+++ app/components/user-index.js
@@ -8,17 +8,11 @@ export default Ember.Component.extend({
   }),
 
   actions: {
-    //TODO: Сделать как надо
-    chooseRole(role){
+    chooseRole(user, test, role){
       const selected = this.set('role', role);
-      console.log(user);
-      // user.set('role', role);
-      // user.save().then((user) => {
-      //   user.set('role', role);
-      // });
-      // console.log(user.get('role.name'));
-      // // console.log(user.get(role).get('name'));
-      // this.get('role.permissions').pushObject(selected);
+      user.set('role', selected);
+      console.log(user.get('role.name'));
+      user.save();
     }
   }
 });
