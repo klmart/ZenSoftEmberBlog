a13460dc5b7c6e617688ad71ddc2ffc325b6401d
klmart <kulbaev.mart@gmail.com>
2016-12-01 11:27:59 +0600

    Refactor

--- app/components/blog-table.js
+++ app/components/blog-table.js
@@ -2,22 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
-  user:  undefined,
 
-  init()
-  {
-    this._super(...arguments);
-
-    //TODO: ???
-    //done
-    this.set('user', this.get('user'));
-  },
-
-  blogs: Ember.computed('user', function () {
-    return this.get('store')
-               .query('blog', {
-                 user: this.get('user.id')
-               });
-  }),
+  blogs: Ember.computed(
+    'user.id',
+    function () {
+      return this.get('store')
+                 .query('blog', {
+                   user: this.get('user.id')
+                 });
+    }
+  ),
 
 });
